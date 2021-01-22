94ae222f84282bf4aff2733a9f3de5984bf8e3f7
uhhhsoyan <eric.essoyan@gmail.com>
2021-01-21 17:19:36 -0800

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://arcane-scrubland-63792.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
